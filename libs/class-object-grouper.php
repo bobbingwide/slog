7597b6e80c9c2dc28a735daa68ee08e40f9e2809
Herb Miller <herb@bobbingwide.com>
2021-01-28 11:22:04 +0000

    Issue #10 - copy necessary libs from slog-bloat

---
+++ libs/class-object-grouper.php
@@ -0,0 +1,644 @@
+<?php
+/**
+ * @copyright (C) Copyright Bobbing Wide 2015-2021
+ * @package wp-top12
+ * Class: Object_Grouper
+ *
+ * Groups objects by key values
+ *
+ * Count all the things we want to count by grouping on key values.
+ *
+ * If we need to convert the actual value of a field into a subset for the grouping
+ * then this should either be done prior to calling the object grouper or it
+ * can be done using a callback function set using subset() ( var $subset ).
+ *
+ * Examples below
+ *
+ *
+ * Key        | Subset | What this shows
+ * ---------- | ------ | ---------------------------------------
+ * downloaded | 10**n  | Grouped by total downloads range
+ * updated    | year   | Grouped by last updated year
+ * slug / name | A-Z   | Grouped by first letter
+ *
+ *
+ */
+class Object_Grouper extends Object_base {
+
+	/**
+	 * @var $groups Array of items for this group.
+	 */
+	public $groups;
+
+	public $key;
+
+	public $subset;
+
+	/**
+	 * Name of the field that holds the elapsed time to accumulate by group.
+	 * Leave null if you don't want this accumulated.
+	 */
+	public $time_field;
+
+	/**
+	 * Array of elapsed time by group. Same key as for $groups.
+	 */
+	public $elapsed;
+
+	/**
+	 * Total number of things counted
+	 */
+	public $total;
+
+	/**
+	 * Total time - when counting time
+	 */
+	public $total_time;
+
+	/**
+	 * Array of percentages rather than counts. Same key as for $groups.
+	 *
+	 */
+	public $percentages;
+
+	public $asCSV_field_percentage_count_accumulative = 0;
+	public $asCSV_field_percentage_elapsed_accumulative = 0;
+
+	/**
+	 * Filter function to perform 'where' like logic
+	 *
+	 * returns true if the object is to be grouped
+	 */
+	public $where;
+
+	public function __construct() {
+		//$this->objects = null;
+		parent::__construct();
+		$this->key();
+		$this->subset();
+		$this->where();
+		$this->having();
+		$this->time_field();
+		$this->reset();
+	}
+
+
+	public function key( $key=null ) {
+		$this->key = $key;
+	}
+
+	public function subset( $subset=null ) {
+		$this->subset = $subset;
+	}
+
+	public function where( $where=null ) {
+		$this->where = $where;
+	}
+
+	public function having( $having=null ) {
+		$this->having = $having;
+	}
+
+
+	/**
+	 * Set the field used for determining elapsed time
+	 */
+	public function time_field( $time_field = null ) {
+		$this->time_field = $time_field;
+		if ( $this->time_field ) {
+			//echo $this->time_field;
+		}
+	}
+
+
+	public function reset() {
+		unset( $this->groups );
+		unset( $this->percentages );
+		unset( $this->elapsed );
+		$this->groups = null;
+		$this->percentages = null;
+		$this->elapsed = null;
+		$this->total = 0;
+		$this->total_time = 0;
+		$this->reset_percentage_accumulatives();
+		//$this->asCSV_field_percentage_count_accumulative = 0;
+		//$this->asCSV_field_percentage_elapsed_accumulative = 0;
+	}
+
+
+	function reset_percentage_accumulatives() {
+		$this->asCSV_field_percentage_count_accumulative = 0;
+		$this->asCSV_field_percentage_elapsed_accumulative = 0;
+	}
+
+	/**
+	 *
+	 */
+	public function groupby( $key, $subset=null ) {
+		//$this->reset();
+		$this->key( $key );
+		if ( $subset ) {
+			$this->subset( $subset );
+		}
+		foreach ( $this->objects as $slug => $object ) {
+			$object = ( object ) $object;
+			if ( $this->where ) {
+				$group = call_user_func( $this->where, $object );
+			} else {
+				$group = true;
+			}
+			if ( $group ) {
+				$this->group( $object );
+			}
+		}
+	}
+
+	/**
+	 * Group this object by its group key value
+	 *
+	 * Allow for there being multiple values returned from the subset method
+	 */
+	public function group( $object ) {
+		//bw_trace2( null, null, true, BW_TRACE_DEBUG );
+		if ( isset( $object->{$this->key} ) ) {
+			$this->field_value = $object->{$this->key};
+		} else {
+			$this->field_value = " ";
+		}
+		if ( $this->subset ) {
+			$this->field_value = call_user_func( $this->subset, $this->field_value );
+		}
+
+		if ( is_array( $this->field_value ) ) {
+			$this->count_values();
+		} else {
+			$this->count_value();
+		}
+
+		if ( $this->time_field ) {
+			$this->accumulate_time( $object );
+		}
+
+	}
+
+	/**
+	 * Report the total values for this group
+	 */
+
+	public function report_total() {
+		echo "Total: " . $this->total . PHP_EOL;
+		if ( $this->time_field ) {
+			echo "Total time: " . $this->total_time . PHP_EOL;
+			if ( $this->total ) {
+				echo "Average: " . $this->total_time / $this->total . PHP_EOL;
+			}
+		}
+	}
+
+	/**
+	 * Initialise groups so that we don't get gaps in the x-axis.
+	 *
+	 */
+	function init_groups( $callback, $low=0, $increment=0.05, $high=6 ) {
+		for ( $elapsed = $low; $elapsed <= $high; $elapsed += $increment ) {
+			$index = call_user_func( $callback, $elapsed );
+			$this->groups[ $index ] = 0;
+		}
+	}
+
+
+
+	/**
+	 * Count this instance
+	 *
+	 */
+	function count_value() {
+		//print_r( $this->field_value );
+		if ( !isset( $this->groups[ $this->field_value ] ) ) {
+			$this->groups[ $this->field_value ] = 1;
+		} else {
+			$this->groups[ $this->field_value ]++;
+		}
+		$this->total++;
+	}
+
+	/**
+	 * Count these instances
+	 */
+	function count_values() {
+		$values = $this->field_value;
+		foreach ( $values as $key => $value ) {
+			$this->field_value = $value;
+			$this->count_value();
+		}
+	}
+
+	/**
+	 * Accumulate the elapsed time for the group
+	 *
+	 * If there is more than one value in the field_value then we accumulate the first value only
+	 * We assume that the time field is set for the $object
+	 *
+	 * @param object $object object which must contain the $time_field property
+	 */
+	function accumulate_time( $object ) {
+
+		if ( is_array( $this->field_value ) ) {
+			$field_value = $this->field_value[0];
+    } else {
+			$field_value = $this->field_value;
+		}
+
+		$time = $object->{$this->time_field};
+		if ( !isset( $this->elapsed[ $field_value ] ) ) {
+			$this->elapsed[ $field_value ] = $time;
+		} else {
+			$this->elapsed[ $field_value] += $time;
+		}
+		$this->total_time += $time;
+
+	}
+
+	/**
+	 * Returns the results for a particular display
+	 * @return string
+	 */
+
+	function asCSV_fields( $display ) {
+		$results = '';
+		foreach ( $this->groups as $key => $field ) {
+			if ( $this->having ) {
+				$having = call_user_func( $this->having, $key, $field );
+			} else {
+				$having = true;
+			}
+			if ( $having ) {
+				$results .= $this->asCSV_field( $key, $field, $display );
+			}
+		}
+		return $results;
+
+	}
+
+	/**
+	 * Returns the results for the table.
+	 *
+	 * That's all the possible sets of values: Count, Elapsed, etc.
+	 * @return string
+	 */
+	function asCSV_table() {
+		$this->reset_percentage_accumulatives();
+		$results = '';
+		foreach ( $this->groups as $key => $field ) {
+			if ( $this->having ) {
+				$having = call_user_func( $this->having, $key, $field );
+			} else {
+				$having = true;
+			}
+			if ( $having ) {
+				$record = [];
+				$record[] = $key;
+				$record[] = $this->asCSV_field_count( $key, $field );
+				$record[] = $this->asCSV_field_elapsed( $key, $field );
+				$record[] = $this->asCSV_field_average( $key, $field );
+				$record[] = $this->asCSV_field_percentage_count( $key, $field );
+				$record[] = $this->asCSV_field_percentage_elapsed( $key, $field );
+				$record[] = $this->asCSV_field_percentage_count_accumulative( $key, $field );
+				$record[] = $this->asCSV_field_percentage_elapsed_accumulative( $key, $field );
+				$results .= implode( ',', $record);
+				$results .= "\n";
+			}
+		}
+		return $results;
+
+	}
+
+	/**
+	 * Returns the display field requested.
+	 * @param $key
+	 * @param $field
+	 *
+	 * @return string
+	 */
+	function asCSV_field( $key, $field, $display ) {
+		$result=$key;
+		$result.=',';
+		$method='asCSV_field_' . $display;
+		if ( method_exists( $this, $method ) ) {
+			$result.=$this->$method( $key, $field );
+		} else {
+			gob();
+		}
+		$result .= "\n";
+		return $result;
+	}
+
+	function asCSV_field_count( $key, $field ) {
+		return $field;
+	}
+
+	function asCSV_field_elapsed( $key, $field ) {
+		if ( isset( $this->elapsed[ $key] ) ) {
+			return $this->elapsed[ $key ];
+		} else {
+			return 0;
+		}
+	}
+
+	/**
+	 * Returns the average elapsed time.
+	 *
+	 * @param $key
+	 * @param $field
+	 *
+	 * @return float|int
+	 */
+	function asCSV_field_average( $key, $field ) {
+		if ( is_array( $field ) ) {
+			$item=implode( " ", $field );
+		} else {
+			$item=$field;
+		}
+		$elapsed = isset( $this->elapsed[ $key ] ) ? $this->elapsed[ $key ] : 0;
+		if ( $item ) {
+			$average=$elapsed / $item;
+		} else {
+			$average = 0;
+		}
+		return $average;
+	}
+
+	function asCSV_field_percentage_count( $key, $field ) {
+		if ( is_array( $field ) ) {
+				gob(); // Not catered for
+		} else {
+			$percentage = ( $field * 100) / $this->total;
+		}
+		//$this->asCSV_field_percentage_count_accumulative += $percentage;
+		return $percentage;
+	}
+
+	function asCSV_field_percentage_elapsed( $key, $field ) {
+		if ( $this->total_time ) {
+			$elapsed = isset( $this->elapsed[ $key ] ) ? $this->elapsed[ $key ] : 0;
+			$percentage = ( $elapsed * 100) / $this->total_time;
+		} else {
+			$percentage = 0;
+		}
+
+		//$this->asCSV_field_percentage_elapsed_accumulative += $percentage;
+		return $percentage;
+	}
+
+	function asCSV_field_percentage_count_accumulative( $key, $field ) {
+		$percentage = ( $field * 100) / $this->total;
+		$this->asCSV_field_percentage_count_accumulative += $percentage;
+		return $this->asCSV_field_percentage_count_accumulative;
+	}
+
+	function asCSV_field_percentage_elapsed_accumulative( $key, $field ) {
+		if ( $this->total_time ) {
+			$elapsed =isset( $this->elapsed[ $key ] ) ? $this->elapsed[ $key ] : 0;
+			$percentage =( $elapsed * 100 ) / $this->total_time;
+			$this->asCSV_field_percentage_elapsed_accumulative+=$percentage;
+		}
+		return $this->asCSV_field_percentage_elapsed_accumulative;
+	}
+
+	function asCSV_count() {
+		$results = '';
+		foreach ( $this->groups as $key => $field ) {
+			if ( $this->having ) {
+				$having = call_user_func( $this->having, $key, $field );
+			} else {
+				$having = true;
+			}
+			if ( $having ) {
+				$results .= $this->asCSV_field( $key, $field );
+			}
+		}
+		return $results;
+
+	}
+
+	function asCSV_elapsed() {
+
+	}
+	/**
+	 * Report the grouped percentages
+	 */
+	function asCSV_percentages() {
+		if ( !$this->percentages ) {
+			$this->percentages();
+		}
+
+		//$this->start_report();
+		//print_r( $this->groups );
+		echo "Groups: " . $this->key . PHP_EOL;
+		$results = '';
+		foreach ( $this->percentages as $key => $field ) {
+
+			if ( $this->having ) {
+				$having = call_user_func( $this->having, $key, $field );
+			} else {
+				$having = true;
+			}
+			if ( $having ) {
+				//$this->report_field( $key, $field );
+				$results .= $this->asCSV_percentage( $key, $field );
+			}
+		}
+		//$this->end_report();
+		return $results;
+	}
+
+
+
+	function asCSV_field_v1( $key, $field ) {
+		//print_r( $field );
+		$string = '';
+		if ( is_array( $field ) ) {
+			$item = implode( " ", $field );
+		} else {
+			$item = $field;
+		}
+
+		//li( "$key $item" );
+		$string .= "$key,$item";
+		if ( $this->time_field ) {
+			$elapsed = $this->elapsed[ $key ] ;
+			$average = $elapsed / $item;
+			$string .=  ",$average";
+		} else {
+
+		}
+		$string .= "\n";
+		return $string;
+	}
+
+
+	function asCSV_percentage( $key, $field ) {
+
+		//li( "$key $item" );
+		$string = "$key,$field";
+		if ( $this->time_field ) {
+			$item = $this->groups[ $key ];
+			$elapsed = $this->elapsed[ $key ] ;
+			$average = $elapsed / $item;
+			//echo ",$elapsed";
+			$string .= ",$average";
+		} else {
+			$string .= "";
+		}
+		$string .= "\n";
+		//echo PHP_EOL;
+		return $string;
+	}
+
+
+	/**
+	 * Report the grouped counts
+	 *
+	 *
+	 */
+	function report_groups() {
+		//$this->start_report();
+		//print_r( $this->groups );
+		//echo "Groups:," . $this->key . PHP_EOL;
+		foreach ( $this->groups as $key => $field ) {
+			if ( $this->having ) {
+				$having = call_user_func( $this->having, $key, $field );
+			} else {
+				$having = true;
+			}
+			if ( $having ) {
+				$this->report_field( $key, $field );
+			}
+		}
+		//$this->end_report();
+	}
+
+	function start_report() {
+		oik_require( "shortcodes/oik-list.php" );
+		$this->uo = bw_sl( $this->atts );
+	}
+
+	function end_report() {
+		bw_el( $this->uo );
+
+	}
+
+	function report_field( $key, $field ) {
+		//print_r( $field );
+		if ( is_array( $field ) ) {
+
+			$item = implode( " ", $field );
+		} else {
+			$item = $field;
+		}
+
+		//li( "$key $item" );
+		echo "$key,$item";
+		if ( $this->time_field ) {
+			$elapsed = $this->elapsed[ $key ] ;
+			$average = $elapsed / $item;
+			//echo ",$elapsed";
+			echo ",$average";
+		} else {
+			echo "";
+		}
+		echo PHP_EOL;
+	}
+
+
+	/**
+	 * Report the grouped percentages
+	 */
+	function report_percentages() {
+		if ( !$this->percentages ) {
+			$this->percentages();
+		}
+
+		//$this->start_report();
+		//print_r( $this->groups );
+		echo "Groups: " . $this->key . PHP_EOL;
+		foreach ( $this->percentages as $key => $field ) {
+
+			if ( $this->having ) {
+				$having = call_user_func( $this->having, $key, $field );
+			} else {
+				$having = true;
+			}
+			if ( $having ) {
+				//$this->report_field( $key, $field );
+				$this->report_percentage( $key, $field );
+			}
+		}
+		//$this->end_report();
+	}
+
+
+	function report_percentage( $key, $field ) {
+
+		//li( "$key $item" );
+		echo "$key,$field";
+		if ( $this->time_field ) {
+			$item = $this->groups[ $key ];
+			$elapsed = $this->elapsed[ $key ] ;
+			$average = $elapsed / $item;
+			//echo ",$elapsed";
+			echo ",$average";
+		} else {
+			echo "";
+		}
+		echo PHP_EOL;
+	}
+
+
+	/**
+	 * Create a group of percentages
+	 */
+	function percentages() {
+		echo "Building percentages: " . $this->key . PHP_EOL;
+		foreach ( $this->groups as $key => $field ) {
+			$this->percentage( $key, $field );
+		}
+	}
+
+	/**
+	 * Calculate a single percentage
+	 */
+	function percentage( $key, $field ) {
+		if ( is_array( $field ) ) {
+			gob(); // Not catered for
+		} else {
+			$percentage = ( $field * 100) / $this->total;
+		}
+		$this->percentages[ $key ] = $percentage;
+	}
+
+	/**
+	 * Sort the groups array
+	 *
+	 * Sort function | Purpose
+	 * ------------- | -----------------
+	 * krsort        | by key descending e.g. 4.4, 4.3, 4.2		or 2016, 2015, 2014
+	 * ksort         | by key ascending e.g. A - Z
+	 * arsort        | by value descending for popularity
+	 * asort         | by value ascending
+	 */
+	function krsort() {
+		krsort( $this->groups );
+	}
+
+	function ksort() {
+		ksort( $this->groups );
+	}
+
+	function arsort() {
+		arsort( $this->groups );
+	}
+
+
+}
+
---
+++ libs/class-vt-row-basic.php
@@ -0,0 +1,319 @@
+<?php
+
+/**
+ * @copyright (C) Copyright Bobbing Wide 2015-2021
+ * @package wp-top12
+ *
+ * VT_row_basic implements a subset of the information for a row from a bwtrace.vt.ccyymmdd file
+ * Actually, it implements a superset!
+ * Silly name for a class therefore.
+ */
+
+
+/**
+ *
+ Array
+(
+    [0] => /oik-plugins/oik/?bwscid1=4&bwscid2=8&bwscid4=3&bwscid5=7
+    [1] =>
+    [2] => 13.834431
+    [3] => 5.3.29
+    [4] => 3160
+    [5] => 4041
+    [6] => 483
+    [7] => 44
+    [8] => 398
+    [9] => 57
+    [10] => 28
+    [11] => 14
+    [12] => 129
+    [13] => 1.07188820839
+    [14] =>
+    [15] => 13.830200
+    [16] => 2015-03-14T00:00:01+00:00
+)
+
+/**
+ * Original version
+ *
+ * 0- request URI
+ * 1 - AJAX action
+ * 2 - elapsed ( final figure )
+ * 3 - PHP version
+ * 4 - PHP functions
+ * 5 - User functions
+ * 6 - Classes
+ * 7 - Plugins
+ * 8 - Files
+ * 9 - Registered Widgets
+ * 10 - Post types
+ * 11 - Taxonomies
+ * 12 - Queries
+ * 13 - Query time
+ * 14 - Trace records
+ * 15 - Elapsed
+ * 16 - Date - ISO 8601 date
+ *
+ * OR it may end
+ *
+ * 14 - Trace records
+ * 15 - Remote address ( IP address )
+ * 16 - Elapsed
+ * 17 - Date - ISO 8601 date
+
+ * OR it may end
+ *
+ * 14 tracefile - which may be null
+ * 15 trace records - a number
+ * 16 - Remote address ( IP address )
+ * 17 - Elapsed
+ * 18 - Date - ISO 8601 date
+*/
+
+
+class VT_row_basic {
+
+//class Trans {
+  //private $trans = null;
+  public $uri;		// $this->trans[0];
+  public $action; // $this->trans[1];
+  public $final ; // $this->trans[2];
+  //public $phpver; // $this->trans[3];
+  //public $phpfns; // $this->trans[4];
+  //public $userfns; // $this->trans[5];
+  public $classes; // $this->trans[6];
+  public $plugins; // $this->trans[7];
+  public $files ; // $this->trans[8];
+  public $widgets; // $this->trans[9];
+  public $types ; // $this->trans[10];
+  public $taxons; // $this->trans[11];
+  public $queries; // $this->trans[12];
+  public $qelapsed; // $this->trans[13];
+  public $tracefile; // $this->trans[14];
+  public $traces; // $this->trans[15] or 14;
+	public $traceerrors; // 16
+	public $hooks; // 17
+	public $remote_IP; // 18
+  public $elapsed; // 19
+  public $isodate; //20
+	public $useragent; // 21
+	public $method; // 22
+
+  public $suri;   // Stripped URI
+	public $suritl; // Top level part of stripped URI
+	public $suril;  // Last part of stripped URI
+
+	public $request_type; // The type of request.
+  //public $qparms; // Query parameters
+	public $narrator;
+	/**
+	 *
+     # | name | Field | sample value
+     - | ---- | ----- |  ------------
+     0 | uri | request | /examples/how-to-create-a-contact-page-using-oik-and-jetpack/jetpack-contact-field-shortcode-country-select-list,
+     1 | action | AJAX action | ,
+     2 | final | Elapsed | 0.309858,
+     3 | phpver | PHP version | 5.6.16,
+     4 | phpfns | PHP functions | 2089,
+     5 | userfns | User functions | 4109,
+     6 | classes | Classes | 378,
+     7 | plugins | Plugins | 41,
+     8 | files | Files | 448,
+     9 | widgets | Registered Widgets | 58,
+     10 | types | Post types | 28,
+     11 | taxons | Taxonomies | 14,
+     12 | queries | Queries | 22,
+     13 | qelapsed | Query time | 0,
+     14 | tracefile | Trace file |  ,
+     15 | traces | Trace records |  ,
+	 16 | traceerrors | Trace errors | ,
+	 17 | hooks | Hook count | ,
+     18 | remote_IP | Remote IP address,68.180.229.222,
+     19 | elapsed | Elapsed time | 0.309793,
+     20 | isodate | Date - ISO 8061 |  2015-12-28T23:57:58+00:00
+	 21 | useragent | HTTP useragent | Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)
+	 22 | method | Request method | GET
+
+		 Other examples
+		 `
+			/oik_api/_bw_acronym/,,0.474451,5.6.16,2089,4512,385,41,488,58,28,14,74,0,,,202.46.56.177,0.474420,2015-12-28T23:59:09+00:00
+			/robots.txt,,0.333942,5.6.16,2089,4162,380,41,451,58,28,14,23,0,,,157.55.39.56,0.333895,2015-12-28T23:59:11+00:00
+		 `
+	*/
+  public function __construct( $transline ) {
+	  $this->narrator = Narrator::instance();
+    $this_trans = str_getcsv( $transline );
+    $this->uri = $this_trans[0];
+    $this->action = $this_trans[1];
+    $this->final  = $this_trans[2];
+    //$this->phpver = $this_trans[3];
+    //$this->phpfns = $this_trans[4];
+    //$this->userfns= $this_trans[5];
+    $this->classes= $this_trans[6];
+    $this->plugins= $this_trans[7];
+    $this->files  = $this_trans[8];
+    $this->widgets= $this_trans[9];
+    $this->types  = $this_trans[10];
+    $this->taxons = $this_trans[11];
+    $this->queries = $this_trans[12];
+    $this->qelapsed = $this_trans[13];
+	$this->tracefile = $this_trans[14];
+    $this->traces = $this_trans[15];
+    $this->traceerrors = $this_trans[16];
+    $this->hooks = $this_trans[17];
+    $this->remote_IP = $this_trans[18];
+    $this->elapsed = $this_trans[19];
+	$this->isodate= $this_trans[20];
+	$this->useragent = $this_trans[21];
+	$this->method = $this_trans[22];
+
+	$this->uri_parser();
+	$this->set_request_type();
+  }
+
+	/**
+	 * Parse the URI
+	 *
+	 *
+	 * Create separate fields
+	 * - stripped URI ( suri )
+	 * - top level ( suritl)
+	 * - bottom level ( suril )
+	 *
+	 */
+	function uri_parser() {
+		//echo $this->uri . PHP_EOL;
+
+    list( $this->suri, $qparms, $blah ) = explode( "?", $this->uri . "???" , 3 );
+		$suri = trim( $this->suri, "/" );
+    $blah = explode( "/", $suri );
+	  $this->suritl = $blah[0];
+		$this->suril = end( $blah );
+		unset( $blah );
+		//echo $this->suritl . PHP_EOL;
+		//echo $this->suril . PHP_EOL;
+
+		//gob();
+	}
+
+	/**
+	 * Sets the request type to help us to try to identify real requests vs bots, spam etc.
+	 * Uses:
+	 * - useragent
+	 * - method
+	 * - suritl
+	 * - action
+	 *
+	 * @TODO - make this a grouping subset callback function like elapsed.
+	 * It could be more efficient.
+	 */
+	function set_request_type() {
+		if ( empty( $this->remote_IP ) ) {
+			$request_type = $this->method . '-CLI';
+		} else {
+			$request_type=$this->method;
+		}
+		$request_type .= $this->is_bot_maybe();
+		$request_type .= $this->front_end_or_admin();
+		$this->request_type = $request_type;
+
+
+
+	}
+
+	/**
+	 * Treats any useragent that contains 'bot' as a BOT.
+	 *
+	 * @return string
+	 */
+	function is_bot_maybe() {
+		$bot = ( false === stripos( $this->useragent, "bot" ) ) ? '' : '-BOT';
+		return $bot;
+	}
+
+	/**
+	 * Attempts to determine the type of request.
+	 *
+	 * Is it normal stuff or spammy?
+	 *
+	 * @return string
+	 */
+
+	function front_end_or_admin() {
+		$feoa = '-FE';
+		if ( $this->action ) {
+			$feoa = '-AJAX';
+			return $feoa;
+		}
+
+		$posdot = strpos( $this->suri, '.');
+		if ( $posdot !== false ) {
+			$feoa = $this->good_or_bad();
+		}
+		return $feoa;
+	}
+
+	/**
+	 * Is this a good or bad request?
+	 *
+	 * Since we're doing a strpos we don't need to trim leading '/'s
+	 * @return string
+	 */
+	function good_or_bad( ) {
+		$feoa = 'FE';
+		$checks = [ 'wp-json' => 'REST',
+					'wp-cli.phar' => 'CLI',
+					'xmlrpc.php' => 'spam',  // More likely to be spam. Could be Jetpack.
+					'wp-cron.php' => 'ADMIN',
+					'sitemap_index.xml' => 'ADMIN',
+					'sitemap' => 'ADMIN',  // Could be a false positive
+					'wp-login.php' => ['spam', 'ADMIN'],
+					'wp-content/uploads' => '404',
+					'wp-content' => 'spam',
+					'favicon.ico' => '404',
+					'.git' => 'spam',
+					'.env' => 'spam',
+					'wp-admin/install.php' => 'spam',
+					'wp-admin/setup-config.php' => 'spam',
+					'wp-admin' => 'ADMIN',
+					'.php' => 'spam',
+					'.js' => 'spam',
+					'.css' => '404',
+					'ads.txt' => 'ADS'  // See Wikis on ads.txt. Could it be a 404? -
+		];
+
+		//$this->narrator->narrate( 'suritl', $this->suritl );
+		//$this->narrator->narrate( 'suril', $this->suri );
+		foreach ( $checks as $search => $indicates ) {
+			if ( false !== strpos( $this->suri, $search ) ) {
+				$this->narrator->narrate_batch( "suri", $this->suri );
+				$feoa=$indicates;
+				$this->narrator->narrate_batch( 'FEOA', $feoa );
+				break;
+			}
+		}
+		if ( is_array( $feoa ) ) {
+			// Then what is it? ADMIN or spam?
+			$feoa = $this->spam_or_admin( $feoa );
+		}
+		return '-' . $feoa;
+
+	}
+
+	/**
+	 * Can we decide what it actually means?
+	 *
+	 * When is it a spam request?
+	 * - If the last request by the same IP is the same?
+	 *
+	 * @param $indicates
+	 *
+	 * @return mixed
+	 */
+	function spam_or_admin( $indicates ) {
+		$feoa = $indicates;
+		// @TODO - write the code that goes here.
+		return $feoa[0];
+	}
+
+}
